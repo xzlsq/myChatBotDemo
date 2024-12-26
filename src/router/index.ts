f2923124d5e9e1bbbf04e037d28863c442c43466
nisekoi <nisekoi@qq.com>
2024-12-26 17:16:48 +0800

    添加聊天界面路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ConversationVIew from '@/views/ConversationVIew.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,12 +9,14 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      redirect: '/home',
-    },
-    {
-      path: '/home',
       component: HomeView,
-    }
+      children: [
+        {
+          path: 'chat/:chatid',
+          component: ConversationVIew,
+        }
+      ]
+    },
   ],
 })
 
