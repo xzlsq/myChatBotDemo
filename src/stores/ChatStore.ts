f045be9c3ccc6bbf8a043ed4fc30fa25a6f4648f
nisekoi <nisekoi@qq.com>
2024-12-26 21:53:12 +0800

    给useChatStore添加记录对话上下文的函数； 添加创建会话的函数； 添加初始化函数；

--- src/stores/ChatStore.ts
+++ src/stores/ChatStore.ts
@@ -1,47 +1,50 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { message } from '@/types'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
-
-  return { count, doubleCount, increment }
-})
-
 type Conversations = {
   chatId: string,
+  title: string,
+  createAt: string,
   history: message[]
 }
 
-var history: Conversations[] = []
-
-if (localStorage.conversations) {
-  history = JSON.parse(localStorage.conversations) as Conversations[]
-} else {
-  history = []
-}
-const useChatStore = defineStore('chatRecord', () => {
-  var conversations = ref<Conversations[]>(history)
+export const useChatStore = defineStore('chatRecord', () => {
+  var conversations = ref<Conversations[]>([])
 
   // 记录某个对话的聊天上下文
   function addDialog(id: string, message: message) {
     var idx =  conversations.value.findIndex(it => it.chatId == id)
     if (idx >= 0) {
+      // 是否是该聊天的首次对话
+      if (conversations.value[idx].history.length == 0) {
+        conversations.value[idx].title = message.content
+      }
       conversations.value[idx].history.push(message)
+      localStorage.conversations = JSON.stringify(conversations.value)
     }
   }
   // 创建一个新对话
   function createConversation() {
-    conversations.value.push({
-      chatId: Math.random().toString(16),
+    conversations.value.unshift({
+      chatId: Math.random().toString(16).slice(2),
+      title: '新的对话',
+      createAt: Date.now().toString(),
       history: []
     })
+    localStorage.conversations = JSON.stringify(conversations.value)
   }
 
-  return {conversations, addDialog, createConversation}
+  function initChatStore() {
+    let conversation: Conversations[] = []
+    if (localStorage.conversations) {
+      conversation = JSON.parse(localStorage.conversations) as Conversations[]
+    } else {
+      conversation = []
+    }
+    conversations.value = conversation
+  }
+
+  return {conversations, addDialog, createConversation, initChatStore}
 
 })
