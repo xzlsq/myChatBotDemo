13917c12bbeba3c3995cbf2ad544847bf9e5b5d6
nisekoi <nisekoi@qq.com>
2024-12-26 17:19:29 +0800

    移除对话功能到新的组件中； 侧边栏显示新建对话的UI和已创建的对话的列表UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,98 +1,23 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import OpenAI from "openai";
-import type { ChatCompletionMessageParam } from 'openai/resources/index.mjs';
 
-type message = {
-  id: string,
-  role: string,
-  content: string
-}
 
-var chatContent = ref<message[]>([])
-var question = ref<string>('')
-
-const openai = new OpenAI({
-  baseURL: 'https://api.deepseek.com',
-  apiKey: 'sk-f0467fd3f70d4d2eb0fbdf49d67127bc',
-  dangerouslyAllowBrowser: true
-});
-
-async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "deepseek-chat",
-  });
-
-  chatContent.value.push({
-    id: Date.now().toString(),
-    role: completion.choices[0].message.role,
-    content: completion.choices[0].message.content ?? ''
-  })
-
-}
-
-function resizeTextarea(e: any) {
-  var textarea = e.target
-  // console.log(textarea.scrollHeight)
-  if (textarea.scrollHeight <= 200) {
-    textarea.classList.add('overflow-hidden')
-    textarea.classList.remove('overflow-auto')
-    textarea.style.height = 'auto'; // 先重置高度
-    textarea.style.height = (textarea.scrollHeight) + 'px';
-  } else {
-    textarea.classList.remove('overflow-hidden')
-    textarea.classList.add('overflow-auto')
-  }
-}
-
-function sendQuestion(e: KeyboardEvent) {
-  // 按下回车开始请求
-  if (e.code == 'Enter' && !e.shiftKey) {
-    e.preventDefault()
-    var message: ChatCompletionMessageParam[] = [{ role: "user", content: question.value }]
-
-    chatContent.value.push({
-      id: Date.now().toString(),
-      role: "user",
-      content: question.value
-    })
-
-    openai.chat.completions.create({
-      messages: message,
-      model: "deepseek-chat",
-    }).then((res) => {
-      chatContent.value.push({
-        id: Date.now().toString(),
-        role: res.choices[0].message.role,
-        content: res.choices[0].message.content ?? ''
-      })
-    })
-    question.value = ''
-  }
-}
-
-main()
 
 </script>
 
 <template>
   <div class="w-full h-full flex">
-    <div class="h-full w-[320px] shrink-0 bg-[#F0F4F9]">
-
+    <div name="对话记录" class="h-full w-[320px] shrink-0 bg-[#F0F4F9] flex flex-col items-center py-14 overflow-hidden gap-2">
+      <div class="bg-gray-300 w-fit p-2 h-12 rounded-full flex items-center justify-center gap-2">
+        <span>➕</span>发起新对话
+      </div>
+      <div class="w-full grow flex flex-col items-center overflow-auto">
+        <RouterLink to="/chat/1" class="w-[90%] rounded-lg bg-slate-300 p-2 [&.router-link-exact-active]:bg-blue-300">
+          新的对话
+        </RouterLink>
+      </div>
     </div>
-    <div class="grow overflow-hidden flex flex-col items-center">
-      <div class="grow w-full mt-14 px-8 pt-4 pb-2 ">
-        <p v-for="content of chatContent" :key="content.id">
-          {{ content.role }}: {{ content.content }}
-        </p>
-      </div>
-      <div class="w-[80%] min-h-16 px-4 py-2 border border-gray-400 bottom-14 
-      rounded flex justify-center items-center mb-14">
-        <textarea @input="(e) => resizeTextarea(e)" v-model="question"
-          class="w-full box-border h-fit resize-none outline-none overflow-hidden"
-          placeholder="问一问... | 按下Shift+Enter换行 | 按下Enter发送" @keypress="(e) => sendQuestion(e)"></textarea>
-      </div>
+    <div name="输出框" class="grow overflow-hidden flex flex-col items-center">
+      <RouterView></RouterView>
     </div>
   </div>
 </template>
