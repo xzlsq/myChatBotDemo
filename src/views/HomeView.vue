f7063a8830b5bab33bd7fe7b530160e44013db8d
nisekoi <nisekoi@qq.com>
2024-12-26 22:26:56 +0800

    实现发起新对话的功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,22 +5,25 @@ import { useRouter } from 'vue-router';
 var ChatStore = useChatStore()
 var router = useRouter()
 
+function createNewConversation() {
+  ChatStore.createConversation()
+  router.replace(`/chat/${ChatStore.conversations[0].chatId}`)
+}
+
 onMounted(() => {
   ChatStore.initChatStore()
   if (ChatStore.conversations.length == 0) {
     ChatStore.createConversation()
   }
-  // console.log(ChatStore.conversations)
-  // router.replace(`/chat/${ChatStore.conversations[0].chatId}`)
 })
 </script>
 
 <template>
   <div class="w-full h-full flex">
     <div name="对话记录" class="h-full w-[320px] shrink-0 bg-[#F0F4F9] flex flex-col items-center py-14 overflow-hidden gap-2">
-      <div class="bg-gray-300 w-fit px-4 py-2 h-12 rounded-full flex items-center justify-center gap-2">
+      <button @click="createNewConversation" class="bg-gray-300 w-fit px-4 py-2 h-12 rounded-full flex items-center justify-center gap-2">
         <span>➕</span>发起新对话
-      </div>
+      </button>
       <div class="w-full grow flex flex-col items-center overflow-auto gap-2">
         <RouterLink :to="`/chat/${chat.chatId}`" v-for="chat of ChatStore.conversations" class="w-[90%] rounded-lg bg-slate-300 p-2 truncate [&.router-link-exact-active]:bg-blue-300">
           {{ chat.title }}
