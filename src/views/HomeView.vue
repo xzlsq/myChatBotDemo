bc7949eeb31c901a546ef18a77af2c72e9c9bdfa
nisekoi <nisekoi@qq.com>
2024-12-26 22:20:31 +0800

    如果数据库中不存在对话记录则自动创建一个对话

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,8 +7,10 @@ var router = useRouter()
 
 onMounted(() => {
   ChatStore.initChatStore()
-  ChatStore.createConversation()
-  console.log(ChatStore.conversations)
+  if (ChatStore.conversations.length == 0) {
+    ChatStore.createConversation()
+  }
+  // console.log(ChatStore.conversations)
   // router.replace(`/chat/${ChatStore.conversations[0].chatId}`)
 })
 </script>
@@ -16,7 +18,7 @@ onMounted(() => {
 <template>
   <div class="w-full h-full flex">
     <div name="对话记录" class="h-full w-[320px] shrink-0 bg-[#F0F4F9] flex flex-col items-center py-14 overflow-hidden gap-2">
-      <div class="bg-gray-300 w-fit p-2 h-12 rounded-full flex items-center justify-center gap-2">
+      <div class="bg-gray-300 w-fit px-4 py-2 h-12 rounded-full flex items-center justify-center gap-2">
         <span>➕</span>发起新对话
       </div>
       <div class="w-full grow flex flex-col items-center overflow-auto gap-2">
